3667d070fbd9a74e70a6ec1c286627d3f58bae47
Yash Patel <yashbpatel2004@gmail.com>
2024-05-29 14:08:55 -0400

    added geo and ip locator

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var moment = require('moment');
+const moment = require('moment');
+const iplocate = require("node-iplocate");
+
+var address;
+var location;
+//getting the ip address of the user
+fetch('https://api.ipify.org')
+    .then((res)=> res.text())
+    .then(ip => {
+        address=ip;
+        iplocate(address).then(function(results){
+            console.log(results);
+            location = results.city +", "+ results.country_code;
+        });
+    })
+    .catch(err=>console.log(err));
 
 function updateTime(){
     var myDate = new Date();
     let day = moment(myDate).format('LL');
     let time = moment(myDate).format('LTS')
-    let location = "Palm Coast, USA";
     let loc = document.getElementById('loc');
     let tyme=document.getElementById('time');
     let dayndate= document.getElementById('dayndate');
